a531af2a54a209a8ea63c3ca34671fa4b788438d
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2023-07-20 11:49:59 -0300

    Add `UnitTestResult`

--- Build/Build.cs
+++ Build/Build.cs
@@ -2,9 +2,14 @@ using Nuke.Common;
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.Tooling;
+using Nuke.Common.Utilities;
 using Nuke.Common.Utilities.Collections;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 class Build : NukeBuild, ITestReport
 {
@@ -18,6 +23,7 @@ public interface ITestReport : IHazIGitHubActions, IHazSolution
         {
             ReportTestCount();
             ReportTestGitHubSummary();
+            ReportTestElement();
         });
 
     AbsolutePath TestResultDirectory => Solution.Directory;
@@ -77,5 +83,119 @@ public interface ITestReport : IHazIGitHubActions, IHazSolution
                 );
         }
     }
+
+    void ReportTestElement()
+    {
+        T DeserializeXml<T>(XElement xElement) where T : class
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using (StringReader reader = new StringReader(xElement.ToString()))
+            {
+                return serializer.Deserialize(reader) as T;
+            }
+        }
+
+        IEnumerable<XElement> GetTestResultElements(AbsolutePath file)
+            => XmlTasks.XmlPeekElements(
+                file,
+                "/xn:TestRun/xn:Results/xn:UnitTestResult",
+                ("xn", "http://microsoft.com/schemas/VisualStudio/TeamTest/2010"));
+
+        var resultFiles = TestResultDirectory.GlobFiles("**\\*.trx");
+
+        foreach (var resultFile in resultFiles)
+        {
+            //Serilog.Log.Information($"{resultFile.Name}");
+
+            GitHubSummaryWriteLine(
+                $"#### {resultFile.Name}",
+                $"```"
+            );
+
+            var testResults = GetTestResultElements(resultFile)
+                .Select(DeserializeXml<UnitTestResult>)
+                .OrderBy(e => e.TestName);
+
+            foreach (var testResult in testResults)
+            {
+                var totalSeconds = (testResult.EndTime - testResult.StartTime).TotalSeconds;
+                var message = $"{testResult.Output?.StdOut} {testResult.Output?.StdErr} {testResult.Output?.ErrorInfo?.Message} {testResult.Output?.ErrorInfo?.StackTrace}";
+                message = message.Trim();
+
+                GitHubSummaryWriteLine(
+                    $"{testResult.Outcome} \t {testResult.TestName} \t {totalSeconds} | {message}"
+                );
+
+                //Serilog.Log.Information($"{testResult.TestName} {testResult.Outcome} {totalSeconds:0.00} | {message}");
+            }
+
+            GitHubSummaryWriteLine(
+                $"```"
+            );
+        }
+    }
+
+
 }
+
+[XmlRoot("UnitTestResult", Namespace = "http://microsoft.com/schemas/VisualStudio/TeamTest/2010")]
+public class UnitTestResult
+{
+    [XmlAttribute("executionId")]
+    public Guid ExecutionId { get; set; }
+
+    [XmlAttribute("testId")]
+    public Guid TestId { get; set; }
+
+    [XmlAttribute("testName")]
+    public string TestName { get; set; }
+
+    [XmlAttribute("computerName")]
+    public string ComputerName { get; set; }
+
+    [XmlAttribute("duration")]
+    public string Duration { get; set; }
+
+    [XmlAttribute("startTime")]
+    public DateTime StartTime { get; set; }
+
+    [XmlAttribute("endTime")]
+    public DateTime EndTime { get; set; }
+
+    [XmlAttribute("testType")]
+    public Guid TestType { get; set; }
+
+    [XmlAttribute("outcome")]
+    public string Outcome { get; set; }
+
+    [XmlAttribute("testListId")]
+    public Guid TestListId { get; set; }
+
+    [XmlAttribute("relativeResultsDirectory")]
+    public string RelativeResultsDirectory { get; set; }
+
+    [XmlElement("Output")]
+    public Output Output { get; set; }
+}
+public class Output
+{
+    [XmlElement("StdOut")]
+    public string StdOut { get; set; }
+
+    [XmlElement("StdErr")]
+    public string StdErr { get; set; }
+
+    [XmlElement("ErrorInfo")]
+    public ErrorInfo ErrorInfo { get; set; }
+}
+public class ErrorInfo
+{
+    [XmlElement("Message")]
+    public string Message { get; set; }
+
+    [XmlElement("StackTrace")]
+    public string StackTrace { get; set; }
+}
+
+
 
